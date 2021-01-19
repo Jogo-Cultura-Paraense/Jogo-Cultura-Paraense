77701088153f62e65a6798a7e33c551a8b968f69
FelipOliveira <sem.ideias.na.hora@gmail.com>
2021-01-19 16:40:46 -0300

    add-icon-buttons

--- jogo_cultura_paraense/lib/pages/home_page.dart
+++ jogo_cultura_paraense/lib/pages/home_page.dart
@@ -7,42 +7,100 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MainMenu(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.info),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          // crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              height: 100,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  MainMenuIconButton(
+                    icon: Icons.share,
+                    onClick: () => _share(context),
+                  ),
+                  MainMenuIconButton(
+                    icon: Icons.info_outline_rounded,
+                    onClick: () => _info(context),
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: MainMenu(),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void _share(BuildContext context) {
+    print('share');
+  }
+
+  void _info(BuildContext context) {
+    print('info');
   }
 }
 
 class MainMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Padding(
-      padding: const EdgeInsets.only(top: 64.0, left: 32.0, right: 32.0),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32.0),
       child: Column(
         children: <Widget>[
           const Text(
             'Jogo Cultura Paraense',
-            style: TextStyle(fontSize: 16.0),
+            style: TextStyle(fontSize: 24.0),
           ),
-          MainMenuButton(label: 'JOGAR'),
-          MainMenuButton(label: 'ENCICLOPÉDIA'),
-          MainMenuButton(label: 'CONFIGURAÇÕES'),
-          MainMenuButton(label: 'SOBRE'),
+          _MainMenuButton(
+            label: 'JOGAR',
+            onClick: () => _game(context),
+          ),
+          _MainMenuButton(
+            label: 'ENCICLOPÉDIA',
+            onClick: () => _enciclopedia(context),
+          ),
+          _MainMenuButton(
+            label: 'CONFIGURAÇÕES',
+            onClick: () => _settings(context),
+          ),
+          _MainMenuButton(
+            label: 'SOBRE',
+            onClick: () => _about(context),
+          ),
         ],
       ),
-    ));
+    );
+  }
+
+  void _game(BuildContext context) {
+    print('game...');
+  }
+
+  void _enciclopedia(BuildContext context) {
+    print('enciclopedia...');
+  }
+
+  void _settings(BuildContext context) {
+    print('settings...');
+  }
+
+  void _about(BuildContext context) {
+    print('about...');
   }
 }
 
-class MainMenuButton extends StatelessWidget {
+// Widget modelo para criar botões no menu
+class _MainMenuButton extends StatelessWidget {
   final String label;
-  final Function function;
+  final Function onClick;
 
-  const MainMenuButton({this.label, this.function});
+  const _MainMenuButton({this.label, this.onClick});
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +110,36 @@ class MainMenuButton extends StatelessWidget {
         width: double.maxFinite,
         child: RaisedButton(
           child: Text(label),
-          onPressed: () => function,
+          onPressed: () => onClick(),
+        ),
+      ),
+    );
+  }
+}
+
+class MainMenuIconButton extends StatelessWidget {
+  final IconData icon;
+  final Function onClick;
+
+  const MainMenuIconButton({Key key, this.icon, this.onClick})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Center(
+        child: Ink(
+          decoration: const ShapeDecoration(
+            color: Colors.red,
+            shape: CircleBorder(),
+          ),
+          child: IconButton(
+              icon: Icon(icon),
+              color: Colors.white,
+              onPressed: () {
+                onClick();
+              }),
         ),
       ),
     );
