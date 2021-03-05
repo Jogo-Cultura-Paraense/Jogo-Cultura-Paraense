191518845861b0197ee3d41fe8c878ba6f441a29
Alberto Sobrinho <aclsobrinho@hotmail.com>
2021-03-04 23:03:04 -0300

    organizing home page and components

--- jogo_cultura_paraense/lib/components/home/home_components.dart
+++ jogo_cultura_paraense/lib/components/home/home_components.dart
@@ -1,3 +1,2 @@
 export 'info_alert.dart';
 export 'main_menu.dart';
-export 'about_alert.dart';
--- jogo_cultura_paraense/lib/components/home/main_menu.dart
+++ jogo_cultura_paraense/lib/components/home/main_menu.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:jogo_cultura_paraense/components/home/about_alert.dart';
+import 'package:jogo_cultura_paraense/components/alert_dialog.dart';
 import 'package:jogo_cultura_paraense/components/main_menu_button.dart';
 import 'package:jogo_cultura_paraense/pages/enciclopedia_page.dart';
 import 'package:jogo_cultura_paraense/pages/game_mode_page.dart';
@@ -14,21 +14,22 @@ class MainMenu extends StatelessWidget {
         const Text(
           'Jogo Cultura Paraense',
           style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              shadows: <Shadow>[
-                Shadow(
-                  offset: Offset(1.0, 1.0),
-                  blurRadius: 2.0,
-                  color: Color.fromARGB(255, 0, 0, 0),
-                ),
-                Shadow(
-                  offset: Offset(1.0, 1.0),
-                  blurRadius: 1.0,
-                  color: Color.fromARGB(255, 0, 0, 0),
-                ),
-              ]),
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            shadows: <Shadow>[
+              Shadow(
+                offset: Offset(1.0, 1.0),
+                blurRadius: 2.0,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+              Shadow(
+                offset: Offset(1.0, 1.0),
+                blurRadius: 1.0,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+            ],
+          ),
         ),
         MainMenuButton(
           label: 'JOGAR',
@@ -69,6 +70,39 @@ class MainMenu extends StatelessWidget {
       builder: (context) {
         return const AboutAlert();
       },
+    );
+  }
+}
+
+class AboutAlert extends StatelessWidget {
+  const AboutAlert({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomAlertDialog(
+      title: null,
+      // Changing the string structure will change how it is displayed
+      content: Text(
+        '''Cultura Paraense Game é um projeto do Laboratório SPIDER que busca o ensinamento dinâmico de cultura paraense de forma lúdica.
+Graduandos: Alberto Sobrinho
+                        Felipe Oliveira
+                        Tuby Neto
+Professor Doutor: Sandro Bezerra''',
+        style: TextStyle(fontSize: 15),
+        textAlign: TextAlign.justify,
+      ),
+      floatingChildHeight: 260,
+      floatingChild: ElevatedButton(
+        child: Text(
+          'Sobre o jogo',
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          primary: Theme.of(context).primaryColor,
+          onPrimary: Theme.of(context).primaryColor,
+        ),
+      ),
     );
   }
 }
