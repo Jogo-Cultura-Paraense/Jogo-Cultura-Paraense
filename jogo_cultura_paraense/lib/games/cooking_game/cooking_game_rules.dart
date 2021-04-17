c222f0c75d412061dd52ec17e6b16aa6aaf88692
Alberto Sobrinho <aclsobrinho@hotmail.com>
2021-04-17 15:08:49 -0300

    add rules for remaining levels

--- jogo_cultura_paraense/lib/games/cooking_game/cooking_game_rules.dart
+++ jogo_cultura_paraense/lib/games/cooking_game/cooking_game_rules.dart
@@ -29,11 +29,106 @@ class CookingGameRules1 extends CookingGameRules {
             Order.fromRecipe(Recipes.patoNoTucupi),
             Order.fromRecipe(Recipes.casquinhaDeCarangueijo),
             Order.fromRecipe(Recipes.tacaca),
+            Order.fromRecipe(Recipes.casquinhaDeCarangueijo),
           ],
           doubleOrders: <Order>[
             Order.fromRecipe(Recipes.patoNoTucupi).doubleOrder(),
-            Order.fromRecipe(Recipes.casquinhaDeCarangueijo).doubleOrder(),
           ],
           time: (4 * 60 + 30), // 4 minutos e 30 segudos
+        );
+}
+
+class CookingGameRules2 extends CookingGameRules {
+  CookingGameRules2()
+      : super(
+          normalOrders: <Order>[
+            Order.fromRecipe(Recipes.patoNoTucupi),
+            Order.fromRecipe(Recipes.casquinhaDeCarangueijo),
+            Order.fromRecipe(Recipes.tacaca),
+            Order.fromRecipe(Recipes.manicoba),
+            Order.fromRecipe(Recipes.patoNoTucupi),
+          ],
+          doubleOrders: <Order>[
+            Order.fromRecipe(Recipes.casquinhaDeCarangueijo).doubleOrder(),
+          ],
+          time: (4 * 60), // 4 minutos
+        );
+}
+
+class CookingGameRules3 extends CookingGameRules {
+  CookingGameRules3()
+      : super(
+          normalOrders: <Order>[
+            Order.fromRecipe(Recipes.patoNoTucupi),
+            Order.fromRecipe(Recipes.casquinhaDeCarangueijo),
+            Order.fromRecipe(Recipes.tacaca),
+            Order.fromRecipe(Recipes.manicoba),
+            Order.fromRecipe(Recipes.filhoteComAcai),
+            Order.fromRecipe(Recipes.tacaca),
+          ],
+          doubleOrders: <Order>[
+            Order.fromRecipe(Recipes.manicoba).doubleOrder(),
+          ],
+          time: (3 * 60 + 30), // 3 minutos e 30 segudos
+        );
+}
+
+class CookingGameRules4 extends CookingGameRules {
+  CookingGameRules4()
+      : super(
+          normalOrders: <Order>[
+            Order.fromRecipe(Recipes.patoNoTucupi),
+            Order.fromRecipe(Recipes.casquinhaDeCarangueijo),
+            Order.fromRecipe(Recipes.tacaca),
+            Order.fromRecipe(Recipes.manicoba),
+            Order.fromRecipe(Recipes.filhoteComAcai),
+            Order.fromRecipe(Recipes.boloDeMacaxeira),
+            Order.fromRecipe(Recipes.manicoba),
+          ],
+          doubleOrders: <Order>[
+            Order.fromRecipe(Recipes.tacaca).doubleOrder(),
+          ],
+          time: (3 * 60), // 3 minutos
+        );
+}
+
+class CookingGameRules5 extends CookingGameRules {
+  CookingGameRules5()
+      : super(
+          normalOrders: <Order>[
+            Order.fromRecipe(Recipes.patoNoTucupi),
+            Order.fromRecipe(Recipes.casquinhaDeCarangueijo),
+            Order.fromRecipe(Recipes.tacaca),
+            Order.fromRecipe(Recipes.manicoba),
+            Order.fromRecipe(Recipes.filhoteComAcai),
+            Order.fromRecipe(Recipes.boloDeMacaxeira),
+            Order.fromRecipe(Recipes.cremeDeCupuacu),
+            Order.fromRecipe(Recipes.filhoteComAcai),
+          ],
+          doubleOrders: <Order>[
+            Order.fromRecipe(Recipes.boloDeMacaxeira).doubleOrder(),
+          ],
+          time: (2 * 60 + 30), // 2 minutos e 30 segudos
+        );
+}
+
+class CookingGameRules6 extends CookingGameRules {
+  CookingGameRules6()
+      : super(
+          normalOrders: <Order>[
+            Order.fromRecipe(Recipes.patoNoTucupi),
+            Order.fromRecipe(Recipes.casquinhaDeCarangueijo),
+            Order.fromRecipe(Recipes.tacaca),
+            Order.fromRecipe(Recipes.manicoba),
+            Order.fromRecipe(Recipes.filhoteComAcai),
+            Order.fromRecipe(Recipes.boloDeMacaxeira),
+            Order.fromRecipe(Recipes.cremeDeCupuacu),
+            Order.fromRecipe(Recipes.vatapa),
+            Order.fromRecipe(Recipes.boloDeMacaxeira),
+          ],
+          doubleOrders: <Order>[
+            Order.fromRecipe(Recipes.filhoteComAcai).doubleOrder(),
+          ],
+          time: (2 * 60), // 2 minutos
         );
 }
