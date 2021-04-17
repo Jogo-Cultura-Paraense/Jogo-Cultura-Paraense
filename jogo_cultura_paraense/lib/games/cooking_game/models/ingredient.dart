e5002d04fa08f936cfc3e4994a03d3bf74d7b899
Alberto Sobrinho <aclsobrinho@hotmail.com>
2021-04-17 15:08:15 -0300

    add remaining ingredients

--- jogo_cultura_paraense/lib/games/cooking_game/models/ingredient.dart
+++ jogo_cultura_paraense/lib/games/cooking_game/models/ingredient.dart
@@ -15,40 +15,105 @@ abstract class Ingredients {
   static Farofa get farofa => Farofa();
   static Camarao get camarao => Camarao();
   static GomaDeTapica get gomaDeTapica => GomaDeTapica();
+  static Maniva get maniva => Maniva();
+  static CarnePorco get carnePorco => CarnePorco();
+  static CarneBoi get carneBoi => CarneBoi();
+  static Filhote get filhote => Filhote();
+  static Acai get acai => Acai();
+  static FarinhaTapioca get farinhaTapioca => FarinhaTapioca();
+  static Macaxeira get macaxeira => Macaxeira();
+  static Acucar get acucar => Acucar();
+  static Ovos get ovos => Ovos();
+  static CocoRalado get cocoRalado => CocoRalado();
+  static Cupuacu get cupuacu => Cupuacu();
+  static LeiteCondensado get leiteCondensado => LeiteCondensado();
+  static CremeLeite get cremeLeite => CremeLeite();
 }
 
 class Pato extends Ingredient {
-  const Pato() : super("pato", imagePath: "0xFF998100");
+  const Pato() : super("pato", imagePath: "0xFFADD8E6");
 }
 
 class Tucupi extends Ingredient {
-  const Tucupi() : super("tucupi", imagePath: "0xFF00007F");
+  const Tucupi() : super("tucupi", imagePath: "0xFF7B68EE");
 }
 
 class Jambu extends Ingredient {
-  const Jambu() : super("jambu", imagePath: "0xFFBADA55");
+  const Jambu() : super("jambu", imagePath: "0xFF7FFFD4");
 }
 
 class Arroz extends Ingredient {
-  const Arroz() : super("arroz", imagePath: "0xFFA936EE");
+  const Arroz() : super("arroz", imagePath: "0xFF8B008B");
 }
 
 class Carangueijo extends Ingredient {
-  const Carangueijo() : super("carangueijo", imagePath: "0xFF631717");
+  const Carangueijo() : super("carangueijo", imagePath: "0xFFBA55D3");
 }
 
 class LeiteDeCoco extends Ingredient {
-  const LeiteDeCoco() : super("leite_coco", imagePath: "0xFFFF8142");
+  const LeiteDeCoco() : super("leite_coco", imagePath: "0xFF1E90FF");
 }
 
 class Farofa extends Ingredient {
-  const Farofa() : super("farofa", imagePath: "0xFFFFC5BE");
+  const Farofa() : super("farofa", imagePath: "0xFF5F9EA0");
 }
 
 class Camarao extends Ingredient {
-  const Camarao() : super("camarao", imagePath: "0xFF2EB9B5");
+  const Camarao() : super("camarao", imagePath: "0xFFDB7093");
 }
 
 class GomaDeTapica extends Ingredient {
-  const GomaDeTapica() : super("goma_tapioca", imagePath: "0xFFCCFF00");
+  const GomaDeTapica() : super("goma_tapioca", imagePath: "0xFFDEB887");
+}
+
+class Maniva extends Ingredient {
+  const Maniva() : super("maniva", imagePath: "0xFFD8BFD8");
+}
+
+class CarnePorco extends Ingredient {
+  const CarnePorco() : super("carne_porco", imagePath: "0xFFFAFAD2");
+}
+
+class CarneBoi extends Ingredient {
+  const CarneBoi() : super("carne_boi", imagePath: "0xFF00008B");
+}
+
+class Filhote extends Ingredient {
+  const Filhote() : super("filhote", imagePath: "0xFF66CDAA");
+}
+
+class Acai extends Ingredient {
+  const Acai() : super("acai", imagePath: "0xFFFF1493");
+}
+
+class FarinhaTapioca extends Ingredient {
+  const FarinhaTapioca() : super("farinha_tapioca", imagePath: "0xFF6495ED");
+}
+
+class Macaxeira extends Ingredient {
+  const Macaxeira() : super("macaxeira", imagePath: "0xFF000000");
+}
+
+class Acucar extends Ingredient {
+  const Acucar() : super("acucar", imagePath: "0xFFB82DC7");
+}
+
+class Ovos extends Ingredient {
+  const Ovos() : super("ovos", imagePath: "0xFF800000");
+}
+
+class CocoRalado extends Ingredient {
+  const CocoRalado() : super("coco_ralado", imagePath: "0xFFFF6347");
+}
+
+class Cupuacu extends Ingredient {
+  const Cupuacu() : super("cupuacu", imagePath: "0xFF8B0000");
+}
+
+class LeiteCondensado extends Ingredient {
+  const LeiteCondensado() : super("leite_condensado", imagePath: "0xFFA9A9A9");
+}
+
+class CremeLeite extends Ingredient {
+  const CremeLeite() : super("creme_leite", imagePath: "0xFFFEECA0");
 }
