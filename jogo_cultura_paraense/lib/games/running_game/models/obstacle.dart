51aedbb359b310f750f74072f3c4babf553fd03c
Alberto Sobrinho <aclsobrinho@hotmail.com>
2021-05-24 23:00:37 -0300

    add obstacle model

---
+++ jogo_cultura_paraense/lib/games/running_game/models/obstacle.dart
@@ -0,0 +1,32 @@
+class Obstacle {
+  final String id;
+  final String imagePath;
+
+  const Obstacle(this.id, [this.imagePath]);
+}
+
+abstract class ObstacleFactory {
+  static Obstacle get aranha {
+    return Obstacle("aranha", "running_game/aranha.png");
+  }
+
+  static Obstacle get onca {
+    return Obstacle("onca", "running_game/onca.png");
+  }
+
+  static Obstacle get pedra {
+    return Obstacle("pedra", "running_game/pedra.png");
+  }
+
+  static Obstacle get poca {
+    return Obstacle("poca", "running_game/poca.png");
+  }
+
+  static Obstacle get sapo {
+    return Obstacle("sapo", "running_game/sapo.png");
+  }
+
+  static Obstacle get tronco {
+    return Obstacle("tronco", "running_game/tronco.png");
+  }
+}
