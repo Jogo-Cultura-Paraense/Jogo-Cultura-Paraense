5f1a98e3a590d3d58ab540da5f25b5c210b54902
Alberto Sobrinho <aclsobrinho@hotmail.com>
2021-05-24 23:19:52 -0300

    add background image to game

--- jogo_cultura_paraense/lib/games/running_game/mainLendasMitos.dart
+++ jogo_cultura_paraense/lib/games/running_game/mainLendasMitos.dart
@@ -4,7 +4,8 @@ import 'package:flutter/widgets.dart';
 import 'dart:async';
 import 'package:jogo_cultura_paraense/games/running_game/gesture_box.dart';
 import 'package:jogo_cultura_paraense/games/running_game/CountDownTimer.dart';
-import 'package:jogo_cultura_paraense/games/running_game/attributes.dart' as globals;
+import 'package:jogo_cultura_paraense/games/running_game/attributes.dart'
+    as globals;
 import 'package:jogo_cultura_paraense/games/running_game/LifeBox.dart';
 import 'package:jogo_cultura_paraense/games/running_game/TargetsBox.dart';
 import 'package:jogo_cultura_paraense/pages/score_page.dart';
@@ -25,16 +26,17 @@ class mainLendasMitosScreen extends State<mainLendasMitos> {
   double score = 100.0;
   LifeBoxScreen temp = LifeBoxScreen.instance;
 
-
   mainLendasMitosScreen._privateConstructor();
-  static final mainLendasMitosScreen _instance = mainLendasMitosScreen._privateConstructor();
+  static final mainLendasMitosScreen _instance =
+      mainLendasMitosScreen._privateConstructor();
   static mainLendasMitosScreen get instance => _instance;
-  static void set instance(value) { instance = null; }
+  static void set instance(value) {
+    instance = null;
+  }
 
   @override
   void initState() {
     super.initState();
-
 
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.landscapeRight,
@@ -64,14 +66,9 @@ class mainLendasMitosScreen extends State<mainLendasMitos> {
 
     super.dispose();
     //Navigator.of(context).popUntil(ModalRoute.withName('/'));
-
-
-
   }
 
   void endGame() {
-
-
     Navigator.of(context).popAndPushNamed(
       ScorePage.routeName,
       arguments: ScorePageArgs(
@@ -88,32 +85,27 @@ class mainLendasMitosScreen extends State<mainLendasMitos> {
     );
   }
 
-
-
   Widget timeBox() {
     return Flexible(
         flex: 15,
         child: FractionallySizedBox(
-          widthFactor: 0.5,
-          heightFactor: 0.15,
-          child: Container(
+            widthFactor: 0.5,
+            heightFactor: 0.15,
+            child: Container(
               decoration: BoxDecoration(
                   color: Colors.red,
                   borderRadius: BorderRadius.all(Radius.circular(5.0))),
               child: Center(
-                child: CountDownTimer(
-                  secondsRemaining: 30,
-                  whenTimeExpires: () {
-                    setState(() {
-                      endGame();
-                    });
-                  },
-
-
+                  child: CountDownTimer(
+                secondsRemaining: 30,
+                whenTimeExpires: () {
+                  setState(() {
+                    endGame();
+                  });
+                },
               )),
-        )));
+            )));
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -123,6 +115,16 @@ class mainLendasMitosScreen extends State<mainLendasMitos> {
     return Scaffold(
         body: Stack(
       children: [
+        Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/running_game/bg.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [TargetsBox(), timeBox(), LifeBox()],
