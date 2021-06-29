9960ef2b6320537fa00f19e3a57eb2b5ea267466
Alberto Sobrinho <aclsobrinho@hotmail.com>
2021-06-29 14:14:54 -0300

    added save connection to vocab level selection

--- jogo_cultura_paraense/lib/pages/level_selection/vocab_level_selection_page.dart
+++ jogo_cultura_paraense/lib/pages/level_selection/vocab_level_selection_page.dart
@@ -1,63 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jogo_cultura_paraense/bloc/save/save_bloc.dart';
 import 'package:jogo_cultura_paraense/components/level_selection/level_selection_card.dart';
-import 'package:jogo_cultura_paraense/components/level_selection/level_selection_page.dart';
+import 'package:jogo_cultura_paraense/components/level_selection/level_selection_scaffold.dart';
 import 'package:jogo_cultura_paraense/games/vocabulario/main_vocabulario.dart';
 import 'package:jogo_cultura_paraense/model/game.dart';
+import 'package:jogo_cultura_paraense/model/game_save.dart';
+import 'package:jogo_cultura_paraense/model/map.dart';
+import 'package:jogo_cultura_paraense/model/map_save.dart';
+import 'package:jogo_cultura_paraense/model/save.dart';
 
-class VocabLevelSelectionPage extends StatelessWidget {
-  static const String routeName = 'vocab_selection_level';
-  const VocabLevelSelectionPage({Key key}) : super(key: key);
-
-  List<LevelSelectionCard> _buildList(BuildContext context) {
-    return <LevelSelectionCard>[
-      LevelSelectionCard(
-        level: 1,
-        unlocked: true,
-        onTap: () {
-          Navigator.of(context).pushNamed(MainVocabularioScreen.routeName);
-        },
-      ),
-      LevelSelectionCard(
-        level: 2,
-        unlocked: true,
-        onTap: () {
-          Navigator.of(context).pushNamed(MainVocabularioScreen.routeName);
-        },
-      ),
-      LevelSelectionCard(
-        level: 3,
-        unlocked: true,
-        onTap: () {
-          Navigator.of(context).pushNamed(MainVocabularioScreen.routeName);
-        },
-      ),
-      LevelSelectionCard(
-        level: 4,
-        unlocked: true,
-        onTap: () {
-          Navigator.of(context).pushNamed(MainVocabularioScreen.routeName);
-        },
-      ),
-      LevelSelectionCard(
-        level: 5,
-        unlocked: true,
-        onTap: () {
-          Navigator.of(context).pushNamed(MainVocabularioScreen.routeName);
-        },
-      ),
-      LevelSelectionCard(
-        level: 6,
-        unlocked: true,
-        onTap: () {
-          Navigator.of(context).pushNamed(MainVocabularioScreen.routeName);
-        },
-      ),
-    ];
-  }
+class VocabLevelSelectionPage extends LevelSelectionScaffold {
+  static const String routeName = '/arq_fest_selection_level';
+  const VocabLevelSelectionPage({Key key})
+      : super(gameName: Games.vocabulary, key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final levels = _buildList(context);
-    return LevelSelectionPage(gameName: Games.vocabulary, levels: levels);
+  List<LevelSelectionCard> buildCards(BuildContext context) {
+    final List<LevelSelectionCard> levels = [];
+    final Save save = BlocProvider.of<SaveBloc>(context).state.currentSave;
+    final List<GameMap> allMaps = Maps.all;
+    MapSave map;
+    GameSave game;
+    bool unlocked;
+    VoidCallback callback;
+    for (int i = 0; i < allMaps.length; i += 1) {
+      map = save.getMapSave(allMaps[i].region);
+      game = map.getGameSave(gameName);
+      unlocked = false;
+      callback = () {};
+      if (map.isOpen && game.isOpen) {
+        unlocked = true;
+        callback = () {
+          Navigator.of(context).pushNamed(MainVocabularioScreen.routeName);
+        };
+      }
+      levels.add(
+        LevelSelectionCard(
+          level: i + 1,
+          unlocked: unlocked,
+          onTap: callback,
+        ),
+      );
+    }
+
+    return levels;
   }
 }
